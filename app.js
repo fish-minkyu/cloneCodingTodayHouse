753c07020cfec44b3a9a3a45dba2182e1365808a
hangju0610 <phj950610@naver.com>
2023-07-22 00:50:35 +0900

    feat : app.use morgan, cors, express.urlencoded 추가

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const express = require('express')
-const dotenv = require('dotenv')
-dotenv.config()
-const app = express()
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
 
-const routes = require('./src/routes/index.route')
+const routes = require('./src/routes/index.route');
+
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  })
+);
+
+app.use(morgan('dev'));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use('/api', routes);
 
 app.listen(process.env.PORT || 3000, (req, res) => {
-    console.log(`${process.env.PORT || 3000} 포트에 접속 되었습니다.`);
-  });
+  console.log(`${process.env.PORT || 3000} 포트에 접속 되었습니다.`);
+});
