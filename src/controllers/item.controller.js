c2220d080a171606f8790be41d086f63264f43fa
Shin Sungyun <sinsungyun1630@gmail.com>
2023-07-23 01:06:27 +0900

    Feat : item 리스트 조회기능 구현

--- src/controllers/item.controller.js
+++ src/controllers/item.controller.js
@@ -1,39 +1,50 @@
-const ItemsService = require("../services/item.service");
+const ItemsService = require('../services/item.service');
 
 class ItemsController {
   itemsService = new ItemsService();
 
   // item 전체 조회
-  findItems = async (req,res,next) => {
+  findItems = async (req, res, next) => {
     try {
       const items = await itemsService.findItems();
       res.status(200).json(items);
     } catch (error) {
-        next(error)
+      next(error);
     }
   };
 
-  // item 카테고리별 리스트
-  findItemsByCategory = async (req, res,next) => {
+  // // item 카테고리별 리스트
+  // findItemsByCategory = async (req, res, next) => {
+  //   try {
+  //     const { query } = req.params;
+  //     const items = await itemsService.findItemsByCategory(query);
+  //     res.status(200).json(items);
+  //   } catch (error) {
+  //     next(error);
+  //   }
+  // };
+
+  // query or category 를 이용한 item 검색
+  findItemsByCategoryOrQuery = async (req, res, next) => {
     try {
-      const { category } = req.params;
-      const items = await itemsService.findItemsByCategory(category);
-      res.status(200).json(items);
+      const query = req.query;
+      const items = await itemsService.findItemsByCategoryOrQuery(query);
+      res.status(200).json({ items });
     } catch (error) {
-        next(error)
+      next(error);
     }
   };
 
   // item 하나보기
-  findItemByItemId = async (req,res,next)=>{
+  findItemByItemId = async (req, res, next) => {
     try {
-        const {itemId}= req.params
-        const item = await itemsService.findItemByItemId(itemId)
-        res.status(200).json(item)
-    }catch(error){
-        next(error)
+      const { itemId } = req.params;
+      const item = await itemsService.findItemByItemId(itemId);
+      res.status(200).json(item);
+    } catch (error) {
+      next(error);
     }
-  }
+  };
 }
 
 module.exports = ItemsController;
