55d2dfa6f57e79ad85f75d3b8ad7a44de4f48c01
hyeong08 <gudtjq9735@gmail.com>
2023-07-22 14:14:45 +0900

    feat : Article Router 작성

---
+++ src/routes/article.route.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const ArticleController = require('../controllers/article.controller');
+const articleController = new ArticleController();
+
+// Article 전체 조회 (추가예정)
+router.get('/');
+// Article 작성
+router.post('/', articleController.createArticle);
+// Article 하나 보기
+router.get('/:articleId', articleController.findArticle);
+// Article 수정
+router.put('/:articleId', articleController.updateArticle);
+// Article 삭제
+router.delete('/:articleId', articleController.deleteArticle);
+
+module.exports = router;
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const articleRouter = require('./article.route');
+// const itemRouter = require('./item.route')
+// const collectionRouter = require('./collection.route')
 
+// Article 관련
+router.use('/api', articleRouter);
+// // Item 관련
+// router.use('/api', itemRouter)
+// // Collection 관련
+// router.use('/api', collectionRouter)
 
 module.exports = router;
