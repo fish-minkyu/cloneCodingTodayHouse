e2c8f1dac84b7d33e487b8bd4881fa55f4a76056
hangju0610 <phj950610@naver.com>
2023-07-22 10:23:55 +0900

    feat : createArticles controller 제작

--- src/controllers/article.controller.js
+++ src/controllers/article.controller.js
@@ -2,6 +2,31 @@ const ArticlesService = require('../services/article.service');
 
 class ArticlesController {
   articlesService = new ArticlesService();
+
+  createArticles = async (req, res, next) => {
+    try {
+      const { userId } = res.locals.user;
+      const { title, coverimage, residence, area, budget, content, tags } =
+        req.body;
+
+      const createArticles = await this.articlesService.createArticles(
+        userId,
+        title,
+        coverimage,
+        residence,
+        area,
+        budget,
+        content,
+        tags
+      );
+
+      res
+        .status(201)
+        .json({ success: true, message: 'Article 작성에 성공하였습니다.' });
+    } catch (error) {
+      next(error);
+    }
+  };
 }
 
 module.exports = ArticlesController;
