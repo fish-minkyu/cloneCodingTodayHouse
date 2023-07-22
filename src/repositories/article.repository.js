bdf653c8aa8cd2412d62f5543c5f279de7a6f5f7
Obligedwalnut <benerogic@gmail.com>
2023-07-22 11:02:59 +0900

    feat : article.repository 제작

--- src/repositories/article.repository.js
+++ src/repositories/article.repository.js
@@ -1,6 +1,29 @@
-const { Articles } = require('../models');
-const { Op } = require('sequelize');
+const { Articles } = require("../models");
+const { Op } = require("sequelize");
 
-class ArticlesRepository {}
+class ArticlesRepository {
+  createArticle = async(
+    userId,
+    title,
+    coverimage,
+    residence,
+    area,
+    budget,
+    content,
+    tags
+  ) => {
+    const createArticleData = await Articles.create({
+        userId,
+        title,
+        coverimage,
+        residence,
+        area,
+        budget,
+        content,
+        tags 
+    })
+    return {success : true}    
+  }
+}
 
 module.exports = ArticlesRepository;
