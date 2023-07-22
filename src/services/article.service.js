1675f2bbd318f312b4a630a92e799ad9ef86faa3
hangju0610 <phj950610@naver.com>
2023-07-22 13:21:33 +0900

    feat:article.service 코드 작성

--- src/services/article.service.js
+++ src/services/article.service.js
@@ -3,6 +3,7 @@ const ArticlesRepository = require('../repositories/article.repository');
 class ArticlesService {
   articlesRepository = new ArticlesRepository();
 
+  // article 생성하기
   createArticle = async (
     userId,
     title,
@@ -13,7 +14,7 @@ class ArticlesService {
     content,
     tags
   ) => {
-    const createArticleData = this.articlesRepository.createArticle(
+    const createArticleData = await this.articlesRepository.createArticle(
       userId,
       title,
       coverimage,
@@ -24,6 +25,40 @@ class ArticlesService {
       tags
     );
     return { success: true };
+  };
+
+  // article 수정하기
+  updateArticle = async (
+    articleId,
+    userId,
+    title,
+    coverimage,
+    residence,
+    area,
+    budget,
+    content,
+    tags
+  ) => {
+    const updateArticleData = await this.articlesRepository.updateArticle(
+      articleId,
+      userId,
+      title,
+      coverimage,
+      residence,
+      area,
+      budget,
+      content,
+      tags
+    );
+    return { success: true };
+  };
+
+  // article 삭제하기
+  deleteArticle = async (userId, articleId) => {
+    const deleteArticleData = await this.articlesRepository.deleteArticle(
+      userId,
+      articleId
+    );
   };
 }
 
