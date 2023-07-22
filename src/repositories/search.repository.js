9623608eb5b8cc6c765226035c58812af013653b
Shin Sungyun <sinsungyun1630@gmail.com>
2023-07-22 19:21:45 +0900

    Docs : 검색 리스트 6개씩 조회

--- src/repositories/search.repository.js
+++ src/repositories/search.repository.js
@@ -18,6 +18,7 @@ class SearchRepository {
           [Op.like]: `%${query}%`,
         },
       },
+      limit: 6,
       raw: true,
     });
 
@@ -29,6 +30,7 @@ class SearchRepository {
           [Op.like]: `%${query}%`,
         },
       },
+      limit: 6,
       //   raw: true,
     });
 
--- src/routes/search.route.js
+++ src/routes/search.route.js
@@ -4,16 +4,7 @@ const router = express.Router();
 const SearchController = require('../controllers/search.controller');
 const searchController = new SearchController();
 
-// 전체 조회 (추가예정)
-router.get('/', searchController.searchData);
-
-// Article 작성
-// router.post('/', articleController.createArticle);
-// // Article 하나 보기
-// router.get('/:articleId', articleController.findArticle);
-// // Article 수정
-// router.put('/:articleId', articleController.updateArticle);
-// // Article 삭제
-// router.delete('/:articleId', articleController.deleteArticle);
+// 통합 검색 조회
+router.get('/', searchController.totalSearchData);
 
 module.exports = router;
