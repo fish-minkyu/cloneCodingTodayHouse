4a43addf50dc9b4bc18154202506eabbc36edef9
fish-minkyu <e951219@naver.com>
2023-07-23 16:42:48 +0900

    Fix 에러문구 수정

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -18,20 +18,13 @@ class LoginController {
       // DB에서 정보 찾아오기
       const user = await this.loginService.findUser(email);
 
-      // DB에서 유저를 찾지 못한 경우 처리
-      if (!user) {
-        return res
-          .status(403)
-          .json({ errorMessage: '유저를 찾을 수 없습니다.' });
-      }
+      // DB에서 email을 찾지 못한 경우 처리
+      if (!user) throw new CustomError("이메일 또는 비밀번호가 일치하지 않습니다.", 403)
+      
       // case. req.body.email과 DB email 일치확인 and req.body.password와 DB password 일치 확인
-      // DB애 등록되지 않는 email이면, 해당 if문에서 error
       const validPassword = await bcrypt.compare(password, user.password);
-      if (!validPassword) {
-        return res
-          .status(400)
-          .json({ errorMessage: '비밀번호가 일치하지 않습니다.' });
-      }
+      if (!validPassword) throw new CustomError("이메일 또는 비밀번호가 일치하지 않습니다.", 403)
+
       // accessToken 생성
       const accessToken = jwt.sign({ email }, 'secretKey', { expiresIn: '1h' });
 
@@ -41,7 +34,7 @@ class LoginController {
       res.cookie('Authorization', `Bearer ${accessToken}`);
       res.status(200).json({ nickname: user.nickname });
     } catch (err) {
-      return res.status(500).json({ errorMessage: '오류가 발생하였습니다.' });
+      throw new CustomError("오류가 발생하였습니다.", 500)
     }
   };
 }
